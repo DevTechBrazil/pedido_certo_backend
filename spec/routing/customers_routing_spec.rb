33a713f152bba7091c6912975e023802d3b01cdc
Leonardo Almeida <leoalmeidasa@gmail.com>
2021-11-11 16:37:15 -0300

    feat(implemented customer create feature): create

--- spec/routing/customers_routing_spec.rb
+++ spec/routing/customers_routing_spec.rb
@@ -5,10 +5,5 @@ RSpec.describe CustomersController, type: :routing do
     it "routes to #index" do
       expect(get: "/customers").to route_to("customers#index")
     end
-
-    it "routes to #create" do
-      expect(post: "/customers").to route_to("customers#create")
-    end
-
   end
 end
