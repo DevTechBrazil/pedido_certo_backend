1751ef12dc75effc511d8305eda1396e4614a79b
Sergio Oseko <osk.sergio@gmail.com>
2021-11-11 06:41:35 -0300

    test: routing customer view test

--- spec/routing/customers_routing_spec.rb
+++ spec/routing/customers_routing_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe CustomersController, type: :routing do
     it "routes to #index" do
       expect(get: "/customers").to route_to("customers#index")
     end
+
+    #expect(get: "/customers/:id").to route_to("customers#show")
+    it { should route(:get, '/customers/1').to(action: :show, id: 1) }
+
   end
 end
