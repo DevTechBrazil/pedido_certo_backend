3b31042fb01bef0aea0d87deff4a11240cd379f8
nicolasbraz <nicolas.braz@gmail.com>
2021-11-19 08:31:52 -0300

    test: destroy test for products

--- spec/routing/products_routing_spec.rb
+++ spec/routing/products_routing_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe ProductsController, type: :routing do
     it "routes to #create" do
       expect(post: "/products").to route_to("products#create")
     end
+
+   it "routes /products/1 to product#destroy" do
+    expect(delete: "/products/1").to route_to(
+      controller: "products",
+      action: "destroy",
+      id: "1"
+    )
   end
 end
