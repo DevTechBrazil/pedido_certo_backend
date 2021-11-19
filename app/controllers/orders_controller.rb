4377888ef62d9ea8adc2912ecb808fce1b68f441
nicolasbraz <nicolas.braz@gmail.com>
2021-11-19 10:17:50 -0300

    feat: created the destroy method and the migrate for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,14 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :update, :destroy]
+
+  def destroy
+    @order.destroy
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_order
+    @order = Order.find(params[:id])
+  end
 end
