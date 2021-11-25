b4f497da7ed5bc0b340d0f6260dfe9dbe3b49479
evandro <ebolzan@inf.ufsm.br>
2021-11-25 11:48:37 -0300

    test: orders update routing

--- spec/routing/orders_routing_spec.rb
+++ spec/routing/orders_routing_spec.rb
@@ -3,7 +3,12 @@ RSpec.describe OrdersController, type: :routing do
  describe "routing" do
     it "routes to #create" do
       expect(post: "/orders").to route_to("orders#create")
-    end    
+    end
+
+    it "routes to #update" do
+      expect(put: "/orders/1").to route_to("orders#update", id: "1")
+    end
+
     it "routes /orders/1 to order#destroy" do
       expect(delete: "/orders/1").to route_to(
       controller: "orders",
