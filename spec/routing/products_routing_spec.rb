e0b42fcae70dc4e32ba8015071940b9b5fe3d00e
evandro <ebolzan@inf.ufsm.br>
2021-11-25 11:13:06 -0300

    test: product update routing

--- spec/routing/products_routing_spec.rb
+++ spec/routing/products_routing_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe ProductsController, type: :routing do
       expect(get: "/products/1").to route_to("products#show", id: "1")
     end
 
+    it "routes to #update" do
+      expect(put: "/products/1").to route_to("products#update", id: "1")
+    end
+
     it "routes to #create" do
       expect(post: "/products").to route_to("products#create")
     end
